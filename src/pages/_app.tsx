6a13f61c110f7585d91bbd0ae649667370532a7c
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-03-28 20:29:39 -0300

    ✨ feat: setup for styled-components and react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
-import type { AppProps } from 'next/app'
+import { useState } from 'react';
+
+import { QueryClient, Hydrate, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
